a10b896c1bb22baf2ac977892f883901005af11d
Jules Kreuer <jules.kreuer@uni-tuebingen.de>
2025-10-19 17:02:16 +0200

    add fastmcp to intersphinx ignore

--- docs/conf.py
+++ docs/conf.py
@@ -126,4 +126,5 @@ nitpick_ignore = [
     # If building the documentation fails because of a missing link that is outside your control,
     # you can add an exception to this list.
     #     ("py:class", "igraph.Graph"),
+    ("py:class", "fastmcp.FastMCP"),
 ]
